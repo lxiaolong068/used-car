f89b88c67542742a262bd71e738eb74f3cd543f2
guoguo <116942619+isunshihan@users.noreply.github.com>
2025-01-26 04:20:04 +0800

    1

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+  },
   reactStrictMode: true,
 }
 
